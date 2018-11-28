e3fc19f5670474dda885feade747fca018f731aa
Brett Sutton <bsutton@noojee.com.au>
2018-11-28 19:01:17 +1100

    merged

--- noojeecontact.api/src/main/java/au/org/noojee/contact/api/SubscriberAdapter.java
+++ noojeecontact.api/src/main/java/au/org/noojee/contact/api/SubscriberAdapter.java
@@ -1,17 +1,7 @@
 package au.org.noojee.contact.api;
 
-<<<<<<< HEAD
 public abstract class SubscriberAdapter implements Subscriber
 {
-=======
-import java.util.List;
-
-public abstract class SubscriberAdapter implements Subscriber
-{
-
-	
->>>>>>> branch 'master' of git@github.com:noojee/noojeecontact.api.git
-
 	@Override
 	public void dialing(EndPoint endPoint, EndPointEvent event)
 	{
